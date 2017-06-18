511062675c33bdb4c9592c6b055f21af10652308
chalab_k <khalis.chalabi@epita.fr>
2017-06-18 15:56:43 +0200

    ProfileViewController: add the choice between movie and tv show

--- Movies/ProfileViewController.swift
+++ Movies/ProfileViewController.swift
@@ -7,19 +7,20 @@
 //
 
 import UIKit
+import Spring
 
-class ProfileViewController: UINavigationController {
+class ProfileViewController: UIViewController {
 
     //MARK: - Properties
+    @IBOutlet weak var myMovieButton: UIButton!
+    @IBOutlet weak var myTvShowButton: UIButton!
+    @IBOutlet weak var myMovieView: SpringView!
+    @IBOutlet weak var myTvShowView: SpringView!
     
-    
-    
-    var isConnected = false
+    var isConnected = true
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
     //Display login page if the user is not connected favorite else
@@ -29,14 +30,32 @@ class ProfileViewController: UINavigationController {
             performSegue(withIdentifier: "connectionSegue", sender: self)
         }
     }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    
+    //MARK: - Actions
+    
+    @IBAction func myMovieAction(_ sender: Any) {
+        myMovieButton.setTitleColor(UIColor.white, for: .normal)
+        myTvShowButton.setTitleColor(UIColor(red: 1.0, green: 1.0, blue: 1.0, alpha: 0.5), for: .normal)
+        myTvShowView.animation = "slideRight"
+        myTvShowView.duration = 1.0
+        myTvShowView.animate()
+        myTvShowView.isHidden = true
+        myMovieView.isHidden = false
+        myMovieView.animation = "slideRight"
+        myMovieView.duration = 1.0
+        myMovieView.animate()
     }
-    */
-
+    
+    @IBAction func myTvShowAction(_ sender: Any) {
+        myTvShowButton.setTitleColor(UIColor.white, for: .normal)
+        myMovieButton.setTitleColor(UIColor(red: 1.0, green: 1.0, blue: 1.0, alpha: 0.5), for: .normal)
+        myMovieView.animation = "slideLeft"
+        myMovieView.duration = 1.0
+        myMovieView.animate()
+        myMovieView.isHidden = true
+        myTvShowView.isHidden = false
+        myTvShowView.animation = "slideLeft"
+        myTvShowView.duration = 1.0
+        myTvShowView.animate()
+    }
 }
